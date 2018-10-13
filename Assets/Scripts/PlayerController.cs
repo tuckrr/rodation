b8f6a314364521978c6209de007ad42ae53e7b8d
tuckrr <ctolsen@vt.edu>
2018-10-12 22:31:53 -0400

    first bit of the controllers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public bool isUp = true;
+    public bool isDown = false;
+
+    private Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        rb.gravityScale = 0;
+        if (isUp) { rb.gravityScale++; }
+        if (isDown) { rb.gravityScale--; }
 	}
 }

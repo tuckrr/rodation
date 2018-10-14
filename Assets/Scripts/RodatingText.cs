38af85a49f44f1e8b1fa5754f3f61c115c6fdbe0
unknown <shill42@vt.edu>
2018-10-14 15:48:05 -0400

    Level 6 added

---
+++ Assets/Scripts/RodatingText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RodatingText : MonoBehaviour {
+
+    float x;
+    void Update()
+    {
+
+        x += Time.deltaTime * 10;
+        transform.rotation = Quaternion.Euler(x, 0, 0);
+
+    }
+}
